fde00f0ee0ce587c57456437833926ce1f03427f
dudaac1 <dudaac99@gmail.com>
2021-04-15 11:48:58 -0300

    Iniciando desenvolvimento

---
+++ trabalho/README.txt
@@ -0,0 +1,28 @@
+# DESCRIÇÃO
+Continuar a sua implementação da agenda (lista1, exercício 3) dentro dos mesmos parâmetros, porém incluir o seguinte:
+Nenhuma variável pode ser declarado em todo o programa, somente ponteiros. Todos os dados do programa devem ser guardados dentro do pBuffer.
+• Exemplo do que não pode: int c; char a; int v[10];
+• Nem mesmo como parâmetro de função. Só ponteiros que apontam para dentro do pBuffer.
+Imprimir de forma ordenada usando uma fila ordenada.
+• O usuário escolhe um parâmetro de ordenação e uma fila ordenada é criada.
+• Depois o programa imprime na tela o conteúdo da fila.
+Implementar a base de dados da agenda usando lista duplamente ligada.
+• Somente essa base de dados pode ficar fora do buffer principal, ou seja, pode usar um malloc para cada nodo.
+
+# DIRETÓRIOS
+    |-- trabalho
+    |   ∟ agenda.c
+
+# EXECUTANDO OS PROGRAMAS (compilação manual)
+Para executar os arquivos, você precisa dos arquivos em seu PC.
+Faça download de cada arquivo .c separadamente ou clone o repositório (git clone https://github.com/dudaac1/aed1-exercicios.git).
+
+Abra o terminal na pasta onde está localizado o arquivo .c.
+Compile o programa (gcc nomeArquivo.c -o nomeArquivo.exe).
+Execute o programa (.\nomeArquivo.exe).
+
+# OBSERVAÇÕES
+Trabalho ainda em desenvolvimento. (Ou seja, provavelmente não tá funcionando.)
+
+___________________________________
+Eduarda A. Carvalho (eduarda.carvalho@inf.ufpel.edu.br).
---
+++ trabalho/agenda.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// protótipos
+void Menu(void);
+void Insert(InfoList * contactList);
+void Delete(InfoList * contactList);
+void Search(InfoList * contactList);
+void List(InfoList * contactList);
+void EndProgram(InfoList * contactList, void * pBuffer);
+void PressEnter(void);
+
+// structs
+typedef struct contact { //dados
+    char name[10];
+    int age;
+    long int phone;
+} Contact; 
+
+typedef struct item { //nodo
+    Contact data;
+    struct item * next;
+    struct item * prev;
+} Item; 
+
+typedef struct info { //sentinela
+    unsigned int quant;
+    Item * first;
+    Item * last;
+} InfoList; 
+
+// principal
+int main() {
+    InfoList * contactList;
+    contactList->quant = 0;
+    contactList->first = NULL;
+
+    void * pBuffer;
+    pBuffer = malloc(sizeof(char));
+    if (pBuffer == NULL)
+    {
+        printf("Desculpa, tivemos um problema.\n");
+        exit(1);
+    }
+
+    char * option = pBuffer;
+    do {
+        Menu();
+        scanf(" %c", option);
+        switch (*option) {
+            case '1':
+                printf("Inserindo contato...\n");
+                Insert(contactList);
+            case '2':
+                printf("Removendo contato...\n");
+                Delete(contactList);
+            case '3':
+                printf("Buscando contato...\n");
+                Search(contactList);
+            case '4':
+                printf("Listando contatos...\n");
+                List(contactList);
+            case '5':
+                EndProgram(contactList, pBuffer);
+                free(pBuffer);
+                exit(0);
+        }
+    } while (*option != '5');
+}
+
+// funções 
+void Menu() {
+    printf("\n\t(1) Adicionar contato\n");
+    printf("\t(2) Remover contato\n");
+    printf("\t(3) Buscar contato\n");
+    printf("\t(4) Listar contatos\n");
+    printf("\t(5) Sair\n");
+    printf("Insira sua escolha: ");
+}
+
+void Insert(InfoList * contactList) {
+    // inserir contato
+}
+
+void Delete(InfoList * contactList) {
+    if (contactList == NULL) {
+        printf("Nenhum contato para deletar.\n");
+    }
+    // remover contato
+}
+
+void Search(InfoList * contactList) {
+    if (contactList == NULL) {
+        printf("Nenhum contato para buscar.\n");
+    }
+    // procurar contato
+}
+
+void List(InfoList * contactList) {
+    if (contactList == NULL) {
+        printf("Nenhum contato para listar.\n");
+    }
+    // listar contatos
+}
+
+void EndProgram(InfoList * contactList, void * pBuffer) {
+    printf("Tchau!\n");
+    // if (contactList != NULL) {
+    //      for (etc etc etc)
+    // }
+    // free nos nodos
+}
+
+void PressEnter() {
+    printf("\nPressione ENTER para continuar... ");
+    getchar();
+    getchar();
+}
