d38025623f10576ce558964510c8ac17202b9c0c
dudaac1 <dudaac99@gmail.com>
2021-05-16 22:38:50 -0300

    finalizando exerc3

--- lista3/README.txt
+++ lista3/README.txt
@@ -1,12 +1,13 @@
 # EXERCICIO 3
 Implemente uma árvore AVL.
-• VERSÃO 1: O programa só pergunta um número para o usuário, que será a quantidade de nós. O programa automaticamente gera n nós em sequência com valor aleatório e vai adicionando na árvore. Use srand(time(0)). Ao final, use um código de verificação (já fornecido) para validar a árvore e dar ok.
-• VERSÃO 2: Após a execução da primeira versão, deve-se testar a árvore com os casos mostrados no seguinte link: https://stackoverflow.com/questions/3955680/how-to-check-if-my-avl-tree-implementation-is-correct
-
+• PARTE 1: O programa só pergunta um número para o usuário, que será a quantidade de nós. O programa automaticamente gera n nós em sequência com valor aleatório e vai adicionando na árvore. Use srand(time(0)). Ao final, use um código de verificação (já fornecido) para validar a árvore e dar ok.
+• PARTE 2: Após a execução da primeira parte, deve-se testar a árvore com os casos mostrados no seguinte link: https://stackoverflow.com/questions/3955680/how-to-check-if-my-avl-tree-implementation-is-correct
 
 # DIRETÓRIOS
     |-- exerc3
     |   ∟ exerc3.c
+    |   ∟ teste1.txt
+    |   ∟ teste2.txt
 
 # EXECUTANDO OS PROGRAMAS (compilação manual)
 Para executar os arquivos, você precisa dos arquivos em seu PC.
@@ -20,9 +21,10 @@ Você também pode utilizar o arquivo de teste.txt presente na pasta do exercíc
 Para isso, baixe o arquivo e, na hora de executar o programa, coloque o seguinte comando: .\nomeArquivo.exe < teste.txt
 
 # OBSERVAÇÕES
-Exercício ainda sendo implementado.
-Versão 1 do exercício em andamento. Função Inserir() não está completamente finalizada devido ao balanceamento da árvore ainda não estar 100% correto.
-
+A parte 1 do exercício pode ser acessado ao chamar a opção 1 do programa. A parte 2, a partir da opção 2. Os arquivos de teste possuem o número referente a opção (ou seja, teste1.txt testa a opção 1).
+A parte 2 foi criada a partir do seguinte post https://stackoverflow.com/a/3958672, sendo realizados, então, inserções que gerassem as 4 rotações, remoções que gerassem as 4 rotações, e uma pesquisa breve que retorna o nodo pesquisado.
+Foram utilizados as constantes TRUE e FALSE e um int de nome BOOLEAN para melhor legibilidade do programa, tendo em vista que, por causa da recursão, tive um pouco de dificuldade inicial para entender o código fornecido pelo professor.
+Utilizei os vídeos sobre árvore AVL do Canal Programação Descomplicada como base para a função de Remover Nodo. Acredito que esta parte poderia estar melhor otimizada.
 
 ___________________________________
 Eduarda A. Carvalho (eduarda.carvalho@inf.ufpel.edu.br).
--- lista3/exerc3/exerc3.c
+++ lista3/exerc3/exerc3.c
@@ -2,72 +2,315 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TRUE 1
+#define FALSE 0
+typedef int BOOLEAN; //para retorno das funções
+
 typedef struct nodo
 {
     int conteudo;
-    struct nodo *pai;
-    struct nodo *filhoEsq, *filhoDir;
+    struct nodo *filhoEsq;
+    struct nodo *filhoDir;
 } Nodo;
 
-Nodo *raiz = NULL; //raiz da árvore criada como global
-
 //protótipos das funções
-void InserirNodo(Nodo **topo, Nodo *novoNodo);
-void balancear(Nodo **nodoRaiz);
+void opcao1();
+void opcao2();
+BOOLEAN inserirNodo(Nodo **nodoAtual, int valor);
+BOOLEAN removerNodo(Nodo **nodoAtual, int valor);
+Nodo *procuraMaior(Nodo *nodoAtual);
+Nodo *pesquisarNodo(Nodo *nodoAtual, int valor);
+BOOLEAN balancear(Nodo **nodoRaiz);
 int calcularFatorBalanc(Nodo *nodoRaiz);
-void balancearEsq(Nodo **nodoRaiz);
-void balancearDir(Nodo **nodoRaiz);
 int alturaArvore(Nodo *nodoAtual);
-void rotarEsq(Nodo **nodoRaiz);
-void rotarDir(Nodo **nodoRaiz);
+BOOLEAN balancearEsq(Nodo **nodoRaiz);
+BOOLEAN balancearDir(Nodo **nodoRaiz);
+void rotarEsq(Nodo **raizAtual);
+void rotarDir(Nodo **raizAtual);
+void imprimirArvore(Nodo *nodoAtual, int nivel);
+BOOLEAN verificaArvoreAVL(Nodo *raizAtual);
+void liberarNodo(Nodo *nodoAtual);
 
 int main()
 {
+    int escolha;
+    printf("Voce deseja criar uma arvore:\n");
+    printf("\t(1) aleatoria pelo numero de nodos.\n");
+    printf("\t(2) com testes pre-definidos.\n");
+    printf("Insira sua escolha: ");
+    scanf("%d", &escolha);
+    while (escolha != 1 && escolha != 2)
+    {
+        printf("Numero invalido. Insira 1 ou 2: ");
+        scanf("%d", &escolha);
+    }
+
+    if (escolha == 1)
+        opcao1();
+    else
+        opcao2();
+
+    printf("\nPressione ENTER para encerrar o programa.\n");
+    getchar();
+}
+
+void opcao1()
+{
+    printf("\n-- OPCAO 1: Criando arvore aleatoria --\n");
+    Nodo *raiz = NULL;
     int nos;
-    printf("Quantos nos voce deseja criar? ");
+    printf("Quantos nodos voce deseja criar? ");
     scanf("%d", &nos);
 
     srand(time(0));
+    printf("Nodos gerados: ");
     for (int i = 0; i < nos; i++)
     {
-        Nodo *novoNodo = (Nodo *)malloc(sizeof(Nodo)); //criando nodo
-        novoNodo->filhoEsq = NULL;
-        novoNodo->filhoDir = NULL;
-        novoNodo->conteudo = (rand() % 100); //numeros de 0 a 100;
-        InserirNodo(&raiz, novoNodo);        //inserindo nodo na árvore
-        //balancear(&raiz); //????
-        printf("Nodo %d inserido.\n", novoNodo->conteudo);
+        int valor = (rand() % 100);               //numeros de 0 a 100;
+        BOOLEAN lixo = inserirNodo(&raiz, valor); //inserindo nodo na árvore
+        printf("%d ", valor);
     }
+    printf("\nPressione ENTER para continuar...\n");
+    getchar();
+    getchar();
+
+    printf("-- IMPRIMINDO ARVORE --\n");
+    imprimirArvore(raiz, 0);
+
+    printf("Arvore eh AVL? ");
+    BOOLEAN ehAvl = verificaArvoreAVL(raiz);
+
+    if (ehAvl)
+        printf("Sim.\n");
+    else
+        printf("Nao.\n");
+
+    //pesquisa
+    int valor;
+    printf("\nInsira valor que deseja buscar: ");
+    scanf("%d", &valor);
+    Nodo *nodoBusca = pesquisarNodo(raiz, valor);
+    if (nodoBusca == NULL)
+        printf("Valor nao esta presente nesta arvore.\n");
+    else
+        printf("Valor presente na arvore: %d\n", nodoBusca->conteudo);
+
+    getchar();
+
+    liberarNodo(raiz);
 }
 
-void InserirNodo(Nodo **nodoAtual, Nodo *novoNodo)
+void opcao2()
 {
-    if (!(*nodoAtual))
-        novoNodo->pai = NULL;
+    printf("\n-- OPCAO 2: Criando arvore com dados pre-definidos --\n");
+    Nodo *raiz = NULL;
 
-    while (*nodoAtual)
-    { //nodoAtual != NULL
-        novoNodo->pai = *nodoAtual;
-        if (novoNodo->conteudo >= (*nodoAtual)->conteudo)
-            nodoAtual = &(*nodoAtual)->filhoDir;
-        else
-            nodoAtual = &(*nodoAtual)->filhoEsq;
-    }
-    *nodoAtual = novoNodo;
-    balancear(&(*nodoAtual)->pai); // balancear a arvore - não tá funcionando direito
-}
-
-void balancear(Nodo **nodoAtual)
-{
-    while (*nodoAtual != NULL)
+    //inserir valores que causem as 4 rotações
+    int valoresInserir[] = {34, 65, 20, 14, 12, 21, 22, 23, 16, 18, 17}; //12 (RSD), 21 (RDD), 23 (RSE), 17 (RDE)
+    printf("Nodos para insercao: ");
+    for (int i = 0; i < 11; i++)
     {
-        int fatorBalanc = calcularFatorBalanc(*nodoAtual);
-        if (fatorBalanc > 1)
-            balancearEsq(nodoAtual);
-        else if (fatorBalanc < -1)
-            balancearDir(nodoAtual);
-        nodoAtual = &(*nodoAtual)->pai;
-    };
+        BOOLEAN lixo = inserirNodo(&raiz, valoresInserir[i]);
+        printf("%d ", valoresInserir[i]);
+    }
+
+    printf("\n\n");
+    imprimirArvore(raiz, 0);
+
+    BOOLEAN ehAvl = verificaArvoreAVL(raiz); //verificando árvore após inserção
+    printf("\nArvore eh AVL? ");
+
+    if (ehAvl)
+        printf("Sim.\n");
+    else
+        printf("Nao.\n");
+
+    printf("\nPressione ENTER para continuar...\n");
+    getchar();
+    getchar();
+
+    //remover valores que causem as 4 rotacoes
+    int valoresRemover[] = {65, 21, 12, 18}; //RSD, RDE, RSE, RDD
+    printf("Nodos para remocao: ");
+    for (int i = 0; i < 4; i++)
+    {
+        BOOLEAN lixo = removerNodo(&raiz, valoresRemover[i]);
+        printf("%d ", valoresRemover[i]);
+    }
+
+    printf("\n\n");
+    imprimirArvore(raiz, 0);
+
+    ehAvl = verificaArvoreAVL(raiz); //verificando árvore após remoção
+    printf("\nArvore eh AVL? ");
+    if (ehAvl)
+        printf("Sim.\n");
+    else
+        printf("Nao.\n");
+
+    //pesquisa
+    int valor;
+    printf("\nInsira valor que deseja buscar: ");
+    scanf("%d", &valor);
+    Nodo *nodoBusca = pesquisarNodo(raiz, valor);
+    if (nodoBusca == NULL)
+        printf("Valor nao esta presente nesta arvore.\n");
+    else
+        printf("Valor presente na arvore: %d\n", nodoBusca->conteudo);
+
+    getchar();
+    liberarNodo(raiz);
+}
+
+BOOLEAN inserirNodo(Nodo **nodoAtual, int valor)
+{
+    if (*nodoAtual == NULL)
+    {
+        *nodoAtual = (Nodo *)malloc(sizeof(Nodo)); //criando nodo
+        (*nodoAtual)->filhoEsq = NULL;
+        (*nodoAtual)->filhoDir = NULL;
+        (*nodoAtual)->conteudo = valor;
+        return TRUE;
+    }
+
+    else if (valor < (*nodoAtual)->conteudo)
+    {                                                    //permite valores repetidos
+        if (inserirNodo(&(*nodoAtual)->filhoEsq, valor)) //if inserirNodo == TRUE
+        {
+            if (balancear(nodoAtual)) //if Balanceamento == TRUE
+                return FALSE;
+            else
+                return TRUE;
+        }
+    }
+    else if (valor >= (*nodoAtual)->conteudo)
+    {
+        if (inserirNodo(&(*nodoAtual)->filhoDir, valor))
+
+            if (balancear(nodoAtual))
+                return FALSE;
+            else
+                return TRUE;
+
+        else
+            return FALSE;
+    }
+}
+
+BOOLEAN removerNodo(Nodo **nodoAtual, int valor)
+{
+    if (*nodoAtual == NULL)
+        return FALSE;
+
+    if (valor < (*nodoAtual)->conteudo)
+    {
+        if (removerNodo(&(*nodoAtual)->filhoEsq, valor))
+        {
+            int fatorBalanc = calcularFatorBalanc(*nodoAtual);
+            fatorBalanc = fatorBalanc >= 0 ? fatorBalanc : fatorBalanc * -1;
+            if (fatorBalanc >= 2)
+            { //comparando altura das arvores do filho a direita
+                if (alturaArvore((*nodoAtual)->filhoDir->filhoEsq) <= alturaArvore((*nodoAtual)->filhoDir->filhoDir))
+                    rotarEsq(nodoAtual); //rotação rr
+                else
+                {                                        //rotação rl = ll + rr
+                    rotarDir(&((*nodoAtual)->filhoDir)); //rotação ll
+                    rotarEsq(nodoAtual);                 //rotação rr
+                }
+                return TRUE;
+            }
+        }
+    }
+    else if (valor > (*nodoAtual)->conteudo)
+    {
+        if (removerNodo(&(*nodoAtual)->filhoDir, valor))
+        {
+
+            int fatorBalanc = calcularFatorBalanc(*nodoAtual);
+            fatorBalanc = fatorBalanc >= 0 ? fatorBalanc : fatorBalanc * -1;
+
+            if (fatorBalanc >= 2)
+            { //comparando altura das arvores do filho a esquerda
+                if (alturaArvore((*nodoAtual)->filhoEsq->filhoDir) <= alturaArvore((*nodoAtual)->filhoEsq->filhoEsq))
+                    rotarDir(nodoAtual); //rotação ll
+                else
+                {                                        //rotação lr = rr + ll
+                    rotarEsq(&((*nodoAtual)->filhoEsq)); //rotação rr
+                    rotarDir(nodoAtual);                 //rotação ll
+                }
+                return TRUE;
+            }
+        }
+    }
+    else
+    { //se valor == nodoAtual->conteudo
+        if ((*nodoAtual)->filhoEsq == NULL || (*nodoAtual)->filhoDir == NULL)
+        { //se tiver 1 filho ou 0 filhos
+            Nodo *nodoRemover = (*nodoAtual);
+            if ((*nodoAtual)->filhoEsq != NULL)
+                *nodoAtual = (*nodoAtual)->filhoEsq;
+            else
+                *nodoAtual = (*nodoAtual)->filhoDir;
+            free(nodoRemover);
+            return TRUE;
+        }
+        else
+        { //se tiver 2 filhos
+            Nodo *aux = procuraMaior((*nodoAtual)->filhoEsq);
+            (*nodoAtual)->conteudo = aux->conteudo;
+            //*nodoAtual = aux; //sera q funciona
+            removerNodo(&(*nodoAtual)->filhoEsq, (*nodoAtual)->conteudo);
+            //removerNodo(&(*nodoAtual)->filhoDir, aux->conteudo); //sera q funciona
+
+            if (calcularFatorBalanc(*nodoAtual) >= 2)
+            {
+                if (alturaArvore((*nodoAtual)->filhoEsq->filhoDir) <= alturaArvore((*nodoAtual)->filhoEsq->filhoEsq))
+                    rotarDir(nodoAtual); //rotação ll
+                else
+                {                                        //rotação lr = rr + ll
+                    rotarEsq(&((*nodoAtual)->filhoEsq)); //rotação rr
+                    rotarDir(nodoAtual);                 //rotação ll
+                }
+                return TRUE;
+            }
+        }
+    }
+    return FALSE;
+}
+
+Nodo *procuraMaior(Nodo *nodoAtual)
+{ //função responsável por tratar a remoção de um nó com 2 filhos
+    Nodo *nodoRetorno = nodoAtual;
+    Nodo *nodoDir = nodoAtual->filhoDir;
+    while (nodoDir != NULL)
+    {
+        nodoRetorno = nodoDir;
+        nodoDir = nodoDir->filhoDir;
+    }
+    return nodoRetorno;
+}
+
+Nodo *pesquisarNodo(Nodo *nodoAtual, int valor)
+{
+    while (nodoAtual != NULL && valor != nodoAtual->conteudo)
+    {
+        if (valor > nodoAtual->conteudo)
+            nodoAtual = nodoAtual->filhoDir;
+        else if (valor < nodoAtual->conteudo)
+            nodoAtual = nodoAtual->filhoEsq;
+    }
+    return nodoAtual;
+}
+
+BOOLEAN balancear(Nodo **nodoAtual)
+{
+    int fatorBalanc = calcularFatorBalanc(*nodoAtual);
+    if (fatorBalanc > 1)
+        return balancearEsq(nodoAtual);
+    else if (fatorBalanc < -1)
+        return balancearDir(nodoAtual);
+    else
+        return FALSE;
 }
 
 int calcularFatorBalanc(Nodo *nodoRaiz)
@@ -80,38 +323,11 @@ int calcularFatorBalanc(Nodo *nodoRaiz)
     return altEsq - altDir;
 }
 
-void balancearEsq(Nodo **nodoRaiz)
-{
-    int fbEsq = calcularFatorBalanc((*nodoRaiz)->filhoEsq);
-    if (fbEsq > 0)
-    {
-        rotarDir(nodoRaiz);
-    }
-    else if (fbEsq < 0)
-    { //rotação dupla direita
-        rotarEsq(&((*nodoRaiz)->filhoEsq));
-        rotarDir(nodoRaiz);
-    }
-}
-
-void balancearDir(Nodo **nodoRaiz)
-{
-    int fbDir = calcularFatorBalanc((*nodoRaiz)->filhoDir);
-    if (fbDir < 0)
-    {
-        rotarEsq(nodoRaiz);
-    }
-    else if (fbDir > 0)
-    { //rotação dupla esquerda
-        rotarDir(&((*nodoRaiz)->filhoDir));
-        rotarEsq(nodoRaiz);
-    }
-}
-
 int alturaArvore(Nodo *nodoAtual)
 {
     if (!nodoAtual)
         return 0;
+
     int alturaEsq = alturaArvore(nodoAtual->filhoEsq);
     int alturaDir = alturaArvore(nodoAtual->filhoDir);
     if (alturaEsq > alturaDir)
@@ -120,32 +336,94 @@ int alturaArvore(Nodo *nodoAtual)
         return alturaDir + 1;
 }
 
-void rotarEsq(Nodo **nodoRaiz)
+BOOLEAN balancearEsq(Nodo **nodoRaiz)
 {
-    Nodo *aux;
-    aux = (*nodoRaiz)->filhoDir;           //aux = filho a direita
-    (*nodoRaiz)->filhoDir = aux->filhoEsq; //filho da direita da raiz = NULL
-    aux->filhoEsq = (*nodoRaiz);           //filho esquerdo de aux = raiz
-
-    //isso não tá funcionando direito, o programa fica num loop depois quando volta para balancear()
-    aux->pai = aux->filhoEsq->pai;
-
-    if (aux->filhoEsq != NULL)
-        aux->filhoEsq->pai = aux; //pai do filho a esq = aux (nova raiz)
-    *nodoRaiz = aux;
+    //a linha abaixo ta detonando meu codigo de remoção
+    int fbEsq = calcularFatorBalanc((*nodoRaiz)->filhoEsq);
+    if (fbEsq > 0)
+    { //rotação simples
+        rotarDir(nodoRaiz);
+        return TRUE;
+    }
+    else if (fbEsq < 0)
+    { //rotação dupla direita
+        rotarEsq(&((*nodoRaiz)->filhoEsq));
+        rotarDir(nodoRaiz);
+        return TRUE;
+    }
+    return FALSE;
 }
 
-void rotarDir(Nodo **nodoRaiz)
+BOOLEAN balancearDir(Nodo **nodoRaiz)
 {
-    Nodo *aux;
-    aux = (*nodoRaiz)->filhoEsq;           //aux - filho a esquerda
-    (*nodoRaiz)->filhoEsq = aux->filhoDir; //filho da direita da raiz = null
-    aux->filhoDir = (*nodoRaiz);           //filho direito de aux = raiz
+    int fbDir = calcularFatorBalanc((*nodoRaiz)->filhoDir);
+    if (fbDir < 0)
+    { //rotação simples
+        rotarEsq(nodoRaiz);
+        return TRUE;
+    }
+    else if (fbDir > 0)
+    { //rotação dupla esquerda
+        rotarDir(&((*nodoRaiz)->filhoDir));
+        rotarEsq(nodoRaiz);
+        return TRUE;
+    }
+    return FALSE;
+}
 
-    //isso não tá funcionando direito, o programa fica num loop depois quando volta para balancear()
-    aux->pai = aux->filhoDir->pai;
+void rotarEsq(Nodo **raizAtual)
+{
+    Nodo *novaRaiz;
+    novaRaiz = (*raizAtual)->filhoDir;           //novaRaiz = filho a direita
+    (*raizAtual)->filhoDir = novaRaiz->filhoEsq; //filho da direita da raiz = NULL
+    novaRaiz->filhoEsq = (*raizAtual);           //filho esquerdo de novaRaiz = raiz
+    *raizAtual = novaRaiz;                       //o endereço da raiz = novaRaiz
+}
 
-    if (aux->filhoDir != NULL)
-        aux->filhoDir->pai = aux; //pai do filho a dir = aux (nova raiz)
-    *nodoRaiz = aux;
+void rotarDir(Nodo **raizAtual)
+{
+    Nodo *novaRaiz;
+    novaRaiz = (*raizAtual)->filhoEsq;           //novaRaiz - filho a esquerda
+    (*raizAtual)->filhoEsq = novaRaiz->filhoDir; //filho da direita da raiz = null
+    novaRaiz->filhoDir = (*raizAtual);           //filho direito de novaRaiz = raiz
+    *raizAtual = novaRaiz;                       //o endereço da raiz = novaRaiz
+}
+
+void imprimirArvore(Nodo *nodoAtual, int nivel)
+{
+    if (nodoAtual == NULL)
+        return;
+
+    for (int i = 0; i < nivel; i++) //imprimindo nivel da arvore; 2 espaços a mais por nivel
+        printf("  ");
+    printf("%d\n", nodoAtual->conteudo); //imprimindo conteudo do nodo
+
+    imprimirArvore(nodoAtual->filhoEsq, nivel + 1);
+    imprimirArvore(nodoAtual->filhoDir, nivel + 1);
+}
+
+BOOLEAN verificaArvoreAVL(Nodo *raizAtual)
+{
+    if (raizAtual == NULL)
+        return TRUE;
+
+    if (!verificaArvoreAVL(raizAtual->filhoEsq))
+        return FALSE;
+    if (!verificaArvoreAVL(raizAtual->filhoDir))
+        return FALSE;
+
+    int fatorBalanc = calcularFatorBalanc(raizAtual);
+    if (fatorBalanc > 1 || fatorBalanc < -1)
+        return FALSE;
+    else
+        return TRUE;
+}
+
+void liberarNodo(Nodo *nodoAtual)
+{
+    if (nodoAtual == NULL)
+        return;
+    liberarNodo(nodoAtual->filhoDir);
+    liberarNodo(nodoAtual->filhoEsq);
+    free(nodoAtual);
 }
