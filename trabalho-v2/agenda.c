e43daf8acd08c200fd0bd7543f4c67d32567c37e
dudaac1 <dudaac99@gmail.com>
2021-09-15 00:13:24 -0300

    inserir 2 pessoas: ok; listar: ok

--- trabalho-v2/agenda.c
+++ trabalho-v2/agenda.c
@@ -21,19 +21,31 @@ void listarOrdenado(void * pBuffer);
 void * fecharPrograma(void * pBuffer);
 void esperePeloEnter(void);
 
+//constantes
+#define NOME (sizeof(char)*10)
+#define CHAR (sizeof(char))
+#define INT (sizeof(int))
+#define TEL (sizeof(long int))
+#define PtoP (sizeof(void*))
+
 // principal
 int main() {
   void * pBuffer;
   // char escolha; char nomeAux[10]; int i; int quantPessoas; void * lista; void * fila;
-  pBuffer = malloc(sizeof(char)*11 + sizeof(int) + sizeof(int) + sizeof(void*) + sizeof(void*));
+  // pBuffer = malloc(sizeof(char)*11 + sizeof(int) + sizeof(int) + sizeof(void*) + sizeof(void*));
+  pBuffer = malloc(CHAR + NOME + (INT*2) + (PtoP*2));
   if (!pBuffer) {
     printf("Desculpe, tivemos um problema.");
     exit(1);
   }
+  // *(int *)(pBuffer + sizeof(char)*11 + sizeof(int)) = 0; // quantPessoas = 0
+  // *(char **)(pBuffer + sizeof(char)*11 + sizeof(int)*2) = NULL; // *nome1Lista = NULL
+  // *(char **)(pBuffer + sizeof(char)*11 + sizeof(int)*2 + sizeof(char *)) = NULL; // *nome1Fila = NULL
 
-  *(int *)(pBuffer + sizeof(char)*11 + sizeof(int)) = 0; // quantPessoas = 0
-  *(char **)(pBuffer + sizeof(char)*11 + sizeof(int)*2) = NULL; // *nome1Lista = NULL
-  *(char **)(pBuffer + sizeof(char)*11 + sizeof(int)*2 + sizeof(char *)) = NULL; // *nome1Fila = NULL
+  *(int *)(pBuffer + CHAR + NOME + INT) = 0; // quantPessoas = 0
+  *(char **)(pBuffer + CHAR + NOME + (INT*2)) = NULL; // *nome1Lista = NULL
+  *(char **)(pBuffer + CHAR + NOME + (INT*2) + PtoP) = NULL; // *nome1Fila = NULL
+
 
   char *escolha = pBuffer;
   do {
@@ -80,55 +92,58 @@ void menu() {
 
 void * inserir(void * pBuffer) {
   // char escolha; char nomeAux[10]; int i; int quantPessoas; char * nome1Lista; char * nome1Fila;
-  int * quantPessoas = (pBuffer + sizeof(char)*11 + sizeof(int));
-  if (*quantPessoas == 0) {
-    printf("lista vazia\n");
-  }
-
+  // int * quantPessoas = (pBuffer + sizeof(char)*11 + sizeof(int));
+  int * quantPessoas = (pBuffer + CHAR + NOME + INT);
 
   //recebendo dados da nova pessoa
   //char nome[10]; int idade; long int telefone; void * proximo; void * anterior;
-  void * pessoa = malloc(sizeof(char)*10 + sizeof(int) + sizeof(long int) + sizeof(void *)*2);
+  // void * pessoa = malloc(sizeof(char)*10 + sizeof(int) + sizeof(long int) + sizeof(void *)*2);
+  void * pessoa = malloc(NOME + INT + TEL + PtoP*2);
   printf("Insira nome: ");
   scanf("%s", (char*)pessoa);
   printf("Insira idade: ");
-  scanf("%d", (int*)(pessoa + sizeof(char)*10));
+  scanf("%d", (int*)(pessoa + NOME));
   printf("Insira telefone:");
-  scanf("%ld", (long int*)(pessoa + sizeof(char)*10 + sizeof(int)));
-  *(void**)(pessoa + sizeof(char)*10 + sizeof(int) + sizeof(long int)) = NULL;
-  *(void**)(pessoa + sizeof(char)*10 + sizeof(int) + sizeof(long int) + sizeof(void*)) = NULL;
+  scanf("%ld", (long int*)(pessoa + NOME + INT));
+  *(void**)(pessoa + NOME + INT + TEL) = NULL;
+  *(void**)(pessoa + NOME + INT + TEL + PtoP) = NULL;
+  getchar();
   
-  if (*quantPessoas == 0) { 
+  printf("ENDERECO DE PESSOA %p\n", &pessoa);
+    printf("EM PESSOA %p\n", pessoa);
+    printf("EM PESSOA* %c\n", *(char*)pessoa);
+
+  if (!(*quantPessoas)) { // se quantPessoas == 0
     //armazenando pessoa na primeira posição da lista e da fila
-    *(void**)(pBuffer + sizeof(char)*11 + sizeof(int)*2) = pessoa;
-    *(void**)(pBuffer + sizeof(char)*11 + sizeof(int)*2 + sizeof(char*)) = pessoa;
+    *(void**)(pBuffer + CHAR + NOME + INT*2) = pessoa;
+    *(void**)(pBuffer + CHAR + NOME + INT*2 + PtoP) = pessoa;
+
+    
   } 
   else { 
     //armazenando pessoa na posição seguinte disponivel
-    void * pessoaAtual = (void**)(pBuffer+ sizeof(char)*11 + sizeof(int)*2); //pessoaAtual = cabeça da lista
-    
-    void * proximo = (void**)(pessoaAtual + sizeof(char)*10 + sizeof(int) + sizeof(long int));
+    void * pessoaAtual = *(void**)(pBuffer + CHAR + NOME + INT*2); //pessoaAtual = cabeça da lista
+    void * proximo = *(void**)(pessoaAtual + NOME + INT + TEL);
 
     while (proximo != NULL) { // ??
       pessoaAtual = proximo;
-      proximo = &*(void**)(proximo + sizeof(char)*10 + sizeof(int) + sizeof(long int));
+      proximo = *(void**)(proximo + NOME + INT + TEL);
     }
 
     // pessoaAtual + sizeof(char)*10 + sizeof(int) + sizeof(long int) = &pessoa;
-    *(void**)(pessoaAtual + sizeof(char)*10 + sizeof(int) + sizeof(long int)) = pessoa; // ??
-
-    
-
+    *(void**)(pessoaAtual + NOME + INT + TEL) = pessoa; // ??
+    printf("\n");
     //testar nomes para inserir na fila em ordem
   }
 
-  printf("char escolha = %c\n", *(char*)(pBuffer));
-  printf("int i = %d\n", *(int*)(pBuffer + sizeof(char)*11));
-  printf("int quantPessoas = %d\n", *(int*)(pBuffer + sizeof(char)*11 + sizeof(int)));
-  printf("void * lista = %p\n", *(void**)(pBuffer+ sizeof(char)*11 + sizeof(int)*2) );
-  printf("void * fila = %p\n", *(void**)(pBuffer+ sizeof(char)*11 + sizeof(int)*2 + sizeof(void*)) );  
 
-  *quantPessoas += 1;
+  // printf("char escolha = %c\n", *(char*)(pBuffer));
+  // printf("int i = %d\n", *(int*)(pBuffer + sizeof(char)*11));
+  // printf("int quantPessoas = %d\n", *(int*)(pBuffer + sizeof(char)*11 + sizeof(int)));
+  // printf("void * lista = %p\n", *(void**)(pBuffer+ sizeof(char)*11 + sizeof(int)*2) );
+  // printf("void * fila = %p\n", *(void**)(pBuffer+ sizeof(char)*11 + sizeof(int)*2 + sizeof(void*)) );  
+
+  (*quantPessoas)++;
     
 
 
@@ -148,9 +163,11 @@ void * buscar(void * pBuffer) {
 
 void listar(void * pBuffer) {
   void * pessoaAtual = *(void **)(pBuffer + sizeof(char)*11 + sizeof(int)*2);
-  printf("%s - ", (char*)pessoaAtual);
-  printf("%d - ", *(int*)(pessoaAtual + sizeof(char)*10));
-  printf("%d", *(int*)(pessoaAtual + sizeof(char)*10 + sizeof(int)));
+  while (pessoaAtual != NULL) {
+    printf("nome: %s - ", (char*)pessoaAtual);
+    printf("\t%d anos - %d\n", *(int*)(pessoaAtual + NOME),  *(long int*)(pessoaAtual + NOME + INT));
+    pessoaAtual = *(void**)(pessoaAtual + NOME + INT + TEL);
+  }
 
   // char * proximo = pessoaAtual;
     // int * proximo = (int*)(pessoaAtual + sizeof(char)*10);
