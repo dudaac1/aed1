e74a09eebadfdcb063dfdf9f5c34fcf3bc2dc369
dudaac1 <dudaac99@gmail.com>
2021-12-04 09:41:53 -0300

    aed1-ex2 refatorado

--- aed1-exerc2/exerc2.c
+++ aed1-exerc2/exerc2.c
@@ -2,219 +2,155 @@
 #include <stdlib.h>
 #include <string.h>
 
-void inserirNome(void);
-void removerNome(void);
-void listarNomes(void);
-void esperePeloEnter(void);
+/*
+EXERCICIO 2
+Programa que armazena nomes, estando TODOS na mesma string.
+O tamanho da string deve crescer/diminuir conforme demanda, sem desperdicio de memoria.
+Utilizar menu: adicionar nome, remover nome, listar, sair.
+*/
 
-char *listaNomes;
-int tamListaNomes = 0;
+// constantes
+#define MAX_NOME 100
 
-int main()
-{
-	listaNomes = malloc(sizeof(char));
-	if (listaNomes == NULL)
-	{
-		printf("Desculpe, tivemos um problema.");
-		exit(1);
-	}
-	
-	char option;
+// estruturas
+typedef struct nome {
+	char * letras;
+	int tamanho;
+} NOME;
+
+// prototipos
+NOME inserir(NOME lista);
+NOME remover(NOME lista);
+void listar(NOME lista);
+NOME lerNome(NOME nome, char * funcao); 
+void erroAloc(void);
+
+// funções
+int main() {
+	NOME lista;
+	lista.letras = NULL;
+	lista.tamanho = 0;	
+	char escolha;
 	do {
-
 		printf("\n\t1) Adicionar nome\n");
 		printf("\t2) Remover nome\n");
-		printf("\t3) Listar\n");
+		printf("\t3) Listar nomes\n");
 		printf("\t4) Sair\n");
 		printf("Insira sua escolha: ");
-		scanf(" %c%*[^\n]", &option);
-		switch (option)
-		{
-		case '1':
-			printf("\n--- ADICIONANDO NOME ---\n");
-			inserirNome();
-			esperePeloEnter();
-			break;
-
-		case '2':
-			printf("\n--- REMOVENDO NOME ---\n");
-			removerNome();
-			esperePeloEnter();
-			break;
-
-		case '3':
-			printf("\n--- LISTANDO NOMES ---\n");
-			listarNomes();
-			esperePeloEnter();
-			break;
-
-		case '4':
-			printf("Tchau!");
-			free(listaNomes);
-			exit(0);
-
-		default:
-			printf("Opcao nao reconhecida.\n");
-			esperePeloEnter();
-		}
-	} while (option != '4');
-}
-
-void inserirNome()
-{
-	getchar();
-	printf("Insira o nome: ");
-	char nomeIn[51] = {""}, letra;
-	int i = 0;
-	while (((letra = getchar()) != '\n') && i < 50)
-	{
-		nomeIn[i] = letra;
-		i++;
-	}
-	if (i == 50) //para remover caracteres excedentes
-	{
-		while ((letra = getchar()) != '\n' && letra != EOF)
-		{
-		}
-		i--;
-	}
-
-	if (strlen(nomeIn) == 0) {
-		printf("Palavras vazias nao sao nomes.\n");
-		return;
-	}
-
-	nomeIn[i] = '#';
-	nomeIn[++i] = '\0';
-	int tamAnterior = tamListaNomes;
-	tamListaNomes += strlen(nomeIn);
-	listaNomes = (char *)realloc(listaNomes, tamListaNomes * sizeof(char));
-	if (listaNomes == NULL)
-	{
-		printf("Desculpe, tivemos um problema.");
-		exit(1);
-	}
-	for (int i = tamAnterior; i < tamListaNomes; i++)
-	{
-		listaNomes[i] = nomeIn[i - tamAnterior];
-	}
-	printf("O nome foi inserido na lista.\n");
-}
-
-void removerNome()
-{
-	getchar();
-	printf("Qual nome voce deseja remover: ");
-	char nomeDel[51] = {""}, letra;
-	int i = 0;
-	while (((letra = getchar()) != '\n') && i < 50)
-	{
-		nomeDel[i] = letra;
-		i++;
-	}
-	
-	if (i == 50)//para remover caracteres excedentes
-	{
-		while ((letra = getchar()) != '\n' && letra != EOF)
-		{
-		}
-		i--;
-	}
-
-	if (strlen(nomeDel) == 0) {
-		printf("Palavras vazias nao sao nomes.\n");
-		return;
-	}
-
-	nomeDel[i] = '#';
-	nomeDel[++i] = '\0';
-
-	int nomeRemovido = 0;
-	for (i = 0; i < tamListaNomes; i++)
-	{
-		if (listaNomes[i] == nomeDel[0])
-		{
-			char *nomeAcum; //para acumular letras iguais ao de nomeDel
-			int tamNomeAcum = 0;
-			nomeAcum = (char *)malloc(sizeof(char));
-			if (nomeAcum == NULL)
-			{
-				printf("Desculpe, tivemos um problema.");
-				exit(1);
-			}
-			int tamNomeDel = strlen(nomeDel);
-			for (int j = i; j < i + tamNomeDel; j++)
-			{
-				if (listaNomes[j] != nomeDel[j - i])
-				{
-					break;
-				}
-
-				tamNomeAcum++;
-				nomeAcum = (char *)realloc(nomeAcum, sizeof(char) * tamNomeAcum + 1); //+1 p drmemory n dar erro
-				if (nomeAcum == NULL)
-				{
-					printf("Desculpe, tivemos um problema.");
-					exit(1);
-				}
-				nomeAcum[tamNomeAcum - 1] = listaNomes[j];
-			}
-			nomeAcum[tamNomeAcum] = '\0';
-			if (strcmp(nomeDel, nomeAcum) == 0)
-			{
-				for (int j = i; j < tamListaNomes - 1; j++)
-				{
-					//se a letra a ser inserida é maior que a quant de letras na lista, quebra o loop
-					if (j + tamNomeDel > tamListaNomes)
-					{
-						break;
-					}
-					listaNomes[j] = listaNomes[j + tamNomeDel];
-				}
-				tamListaNomes -= tamNomeDel;
-				listaNomes = (char *)realloc(listaNomes, sizeof(char) * tamListaNomes);
-				if (listaNomes == NULL && tamListaNomes != 0)
-				{
-					printf("Desculpe, tivemos um problema.");
-					exit(1);
-				}
-				printf("O nome foi removido!\n");
-				nomeRemovido = 1;
-				free(nomeAcum);
+		scanf("%c[^\n]", &escolha);
+		getchar();
+		switch (escolha) {
+			case '1':
+				lista = inserir(lista);
 				break;
-			}
-			free(nomeAcum);
+			case '2':
+				lista = remover(lista);
+				break;
+			case '3':
+				listar(lista);
+				break;
+			case '4':
+				printf("Tchau!");
+				free(lista.letras); 
+				exit(0);
+			default:
+				printf("Opcao nao reconhecida.\n");
 		}
-	}
-	if (nomeRemovido == 0 && i == tamListaNomes)
-	{
-		printf("Nenhum nome semelhante foi encontrado para remocao.\n");
-	}
+		printf("\nPressione ENTER para continuar... ");
+		getchar();	
+	} while (escolha != '4');
 }
 
-void listarNomes()
-{
-	getchar();
-	if (tamListaNomes == 0) {
+NOME inserir(NOME lista) {
+	printf("\n--- ADICIONANDO NOME ---\n");
+	NOME nome = lerNome(nome, "inserir");
+
+	if (nome.tamanho == 1) { // "#"
+		printf("Palavras vazias nao sao nomes.\n");
+		free(nome.letras);
+		return lista;
+	}
+
+	lista.tamanho += nome.tamanho;
+	lista.letras = (char *)realloc(lista.letras, lista.tamanho * sizeof(char));
+	if (lista.letras == NULL) erroAloc();
+	for (int i = lista.tamanho - nome.tamanho, j = 0; i < lista.tamanho; i++, j++)
+		lista.letras[i] = nome.letras[j];
+	free(nome.letras);
+	printf("O nome foi inserido na lista.\n");
+	return lista;
+}
+
+NOME remover(NOME lista) {
+	printf("\n--- REMOVENDO NOME ---\n");
+	NOME nome = lerNome(nome, "remover");
+
+	if (nome.tamanho == 1) { // "#"
+		printf("Palavras vazias nao sao nomes.\n");
+		free(nome.letras);
+		return lista;
+	}
+
+	char * pontNome = strstr(lista.letras, nome.letras);
+	if (pontNome == NULL) {
+		printf("Nome nao encontrado.\n");
+		free(nome.letras);
+		return lista;
+	}
+
+	int tamResto = strlen(pontNome) - nome.tamanho;
+	lista.tamanho -= nome.tamanho;
+	if (lista.tamanho != 0) {
+		int i;
+		for (i = 0; i < tamResto; i++) 
+			pontNome[i] = pontNome[i + nome.tamanho];
+		pontNome[i] = '\0';
+	}
+
+	lista.letras = (char *)realloc(lista.letras, (lista.tamanho+1) * sizeof(char));
+	free(nome.letras);
+	printf("Nome removido.\n");
+	return lista;
+}
+
+void listar(NOME lista) {
+	printf("\n--- LISTANDO NOMES ---\n");
+	if (lista.tamanho == 0) {
 		printf("Nenhum nome encontrado.\n");
 		return;
 	}
-
-	for (int i = 0; i < tamListaNomes; i++)
+	for (int i = 0; i < lista.tamanho; i++)
 	{
-		if (listaNomes[i] != '#')
-		{
-			printf("%c", listaNomes[i]);
-		}
+		if (lista.letras[i] != '#')
+			printf("%c", lista.letras[i]);
 		else
-		{
 			printf("\n");
-		}
 	}
 }
 
-void esperePeloEnter()
-{
-	printf("\nPressione ENTER para continuar... ");
-	getchar();
+NOME lerNome(NOME nome, char * funcao) {
+	nome.letras = (char *)malloc(sizeof(char) * MAX_NOME+1);
+	if (nome.letras == NULL) erroAloc();
+	nome.tamanho = 0;
+
+	printf("Qual nome voce deseja %s: ", funcao);
+ 	char letra;
+	for (int i = 0; (letra = getchar()) != '\n'; i++) 
+		if (nome.tamanho < MAX_NOME) 
+			nome.letras[nome.tamanho++] = letra;
+
+	if (nome.tamanho == MAX_NOME) {
+		nome.letras[MAX_NOME-1] = '#';
+		nome.letras[MAX_NOME] = '\0';
+	} else {
+		nome.letras[nome.tamanho++] = '#';
+		nome.letras[nome.tamanho] = '\0';
+	}
+
+	return nome;
+}
+
+void erroAloc() {
+	printf("Desculpe, tivemos um problema.");
+	exit(1);
 }
